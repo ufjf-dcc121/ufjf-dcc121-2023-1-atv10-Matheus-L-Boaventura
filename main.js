9d0f32caaf25075aea12f25ce1d764cab98cbded
Matheus Luz Boaventura <matheusluz.boaventura@estudante.ufjf.br>
2023-05-23 20:39:43 -0300

    configura por evento

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+window.addEventListener('load', setup);
+
+function setup(){
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
+}
 
 function evenvia(evento) {
     evento.preventDefault();
