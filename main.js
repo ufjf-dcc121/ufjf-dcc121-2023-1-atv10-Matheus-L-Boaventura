017e693f4c5a9d285db1435117183ff755db8ed3
Matheus Luz Boaventura <matheusluz.boaventura@estudante.ufjf.br>
2023-05-30 19:29:18 -0300

    add um estado basico

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-window.addEventListener('load', setup);
 
-function setup(){
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
-}
+
+let estado = 0;
 
 function evenvia(evento) {
     evento.preventDefault();
     console.log('Formulário enviado!');
+    estado++;
+    atualiza();
+}
+
+function atualiza(){
+    const ol = document.querySelector('ol');
+    ol.innerHTML = `<li>${estado}</li>`;
 }
